1c9428829acb4cc2ef11215a7ef338b3604245c1
Yifaht Meir <yifahtmyers@gmail.com>
2024-03-10 18:16:04 +0200

    Add app header&nav bar

--- react-miss-book/RootCmp.jsx
+++ react-miss-book/RootCmp.jsx
@@ -5,7 +5,7 @@ import { Home } from './pages/Home.jsx'
 import { About } from './pages/About.jsx'
 import { BookIndex } from './pages/BookIndex.jsx'
 
-// import { AppHeader } from './cmps/AppHeader.jsx'
+import { AppHeader } from './cmps/AppHeader.jsx'
 
 export function RootCmp() {
     const [page, setPage] = useState('book')
@@ -14,7 +14,7 @@ export function RootCmp() {
 
     return (
         <section className="app main-layout">
-            {/* <AppHeader setPage={setPage} /> */}
+            <AppHeader setPage={setPage} />
             <main className="full main-layout">
                 {page === 'home' && <Home />}
                 {page === 'about' && <About />}
