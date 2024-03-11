bce24eef96533d6bfd7acad0b693827efba93e3b
Yifaht Meir <yifahtmyers@gmail.com>
2024-03-11 07:06:12 +0200

    Add color to prices

--- react-miss-book/cmps/BookPreview.jsx
+++ react-miss-book/cmps/BookPreview.jsx
@@ -2,7 +2,6 @@
 export function BookPreview({ book }) {
     return <article className="book-preview">
         <h2>{book.title}</h2>
-        <h5>Number of pages : {book.maxPages}</h5>
         <img src={`${book.thumbnail}`} />
     </article>
 }
